142f713cd8e8ef53af10ca65c6809ae93dc797d9
jdhalmahapatra <jyotiraditya12@hotmail.com>
2022-07-30 00:42:13 +0530

    Update the last commit files with bean scope especially Singleton and Prototype types

--- springcore/src/main/java/com/springcore/stereotype/annotation/Program.java
+++ springcore/src/main/java/com/springcore/stereotype/annotation/Program.java
@@ -13,6 +13,22 @@ public class Program {
 		Student std = context.getBean("student", Student.class);
 		System.out.println(std);
 		System.out.println(std.getCityAdresses());
+		
+		
+		System.out.println(std.hashCode());  //by default Singleton bean scope
+		
+		Student std1 = context.getBean("student", Student.class);
+		System.out.println(std1.hashCode()); //by default Singleton bean scope
+		
+		
+		Teacher t1 = context.getBean("teacher", Teacher.class);
+		System.out.println(t1.getTeacherName());
+		
+		System.out.println(t1.hashCode());
+		
+		Teacher t2 = context.getBean("teacher", Teacher.class);
+		System.out.println(t2.hashCode());
+		
 		((ClassPathXmlApplicationContext)context).close();
 	}
 
---
+++ springcore/src/main/java/com/springcore/stereotype/annotation/Teacher.java
@@ -0,0 +1,17 @@
+package com.springcore.stereotype.annotation;
+
+public class Teacher {
+	private String teacherName;
+
+	public String getTeacherName() {
+		return teacherName;
+	}
+
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+
+	
+	
+	
+}
